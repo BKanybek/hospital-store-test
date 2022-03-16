2aee6440e9e9740cdb47dbfcd1a2b4d5a739842a
KBolotkanov <westbek9@gmail.com>
2022-03-16 18:29:26 +0600

    9test

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
+
 const PORT = process.env.PORT || 8000;
-
 server.use(middlewares);
-server.use(router);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
 
+server.use(router);
 server.listen(PORT, () => {
   console.log('Server is running');
 });
