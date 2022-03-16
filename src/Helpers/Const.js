690d91f97e5a9f9d90761f439290d7f0372bcfbc
KBolotkanov <westbek9@gmail.com>
2022-03-16 16:14:40 +0600

    7test

--- src/Helpers/Const.js
+++ src/Helpers/Const.js
@@ -1,3 +1,3 @@
-export const API = "https://hospital-store.herokuapp.com/api"
-export const API2 = "https://hospital-store.herokuapp.com/api"
-export const API_COMMENTS = "https://hospital-store.herokuapp.com/api"
+export const API = "https://hospital-store.herokuapp.com/products/api"
+export const API2 = "https://hospital-store.herokuapp.com/equipments/api"
+export const API_COMMENTS = "https://hospital-store.herokuapp.com/comments/api"
