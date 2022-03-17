af9c460538ccd7f45d0f8c26e5b1f00bd85f4e95
BKanybek <95636162+BKanybek@users.noreply.github.com>
2022-03-17 14:49:12 +0400

    1234567

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const middlewares = jsonServer.defaults({
 });
 const PORT = process.env.PORT || 8000;
 server.use(middlewares);
-// server.use(jsonServer.rewriter({
-//   '/api/*': '/$1',
-// }))
-server.use("api",router);
+
+
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}), router);
+
+
+// server.use("/api",router);
+
 server.listen(PORT, () => {
   console.log('Server is running');
 });
